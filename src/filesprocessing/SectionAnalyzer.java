dda54c6bee40f0beea96d771462f8f8dc0fade11
Xelanos <or3232@gmail.com>
2017-05-29 20:11:23 +0300

    changed to warning format required. now with space...

--- src/filesprocessing/SectionAnalyzer.java
+++ src/filesprocessing/SectionAnalyzer.java
@@ -40,7 +40,7 @@ class SectionAnalyzer {
                 manipulator = manipulatorFactory.getCommandManipulator(commands);
             }
             catch (FirstException e){
-                System.out.println("Warning in line"+sectionNum*(i+1));
+                System.out.println("Warning in line "+sectionNum*(i+1));
                 manipulator = null;
             }
             sectionManipulators[i/2] = manipulator;
